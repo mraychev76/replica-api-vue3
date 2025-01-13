1a82b907767ce00ce2686d1b6a18f0a204029129
mraychev76 <m.raychev@nuxgame.com>
2025-01-13 15:34:17 +0200

    Get Providers Tests

---
+++ cypress/e2e/get-providers.cy.js
@@ -0,0 +1,32 @@
+// GET Providers List
+
+const password = 'Qwerty1Admin*'
+const user_id = 'maxraychev76@gmail.com'
+const platform_id = '28fcaa6c-7147-457b-824c-391a95038d96'
+const login_url = "/api/users/authorize"
+const get_providers_url ='/api/v2/casino/spins/get-providers'
+let token = ''
+
+
+describe('Get Providers List Tests', () => {
+    before(() => {
+        cy.authorize(login_url, password, user_id, platform_id).then(({body}) => {
+            token = body.data.token
+        })
+    })
+    it('Should Return List Of Game Providers', () => {
+        cy.request({
+            methtod: 'GET',
+            url: get_providers_url,
+            headers: {
+                'Platform-Id': platform_id
+            },
+            auth: {
+                bearer: token
+            }
+        }).then(({status, body}) => {
+            expect(status).equal(200)
+            expect(body.status).equal(true)
+        })
+    })
+})
